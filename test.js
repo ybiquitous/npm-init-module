aec8ea80083f868cb5ec86b029aa9c6a8ba51eec
Masafumi Koba <ybiquitous@gmail.com>
2018-09-05 01:10:24 +0900

    test: fix unsupported `console.debug()` on Node 6

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const run = cmd => {
-  console.debug(`> ${cmd}`);
+  console.log(`> ${cmd}`);
   execSync(cmd, { stdio: "inherit" });
 };
 
